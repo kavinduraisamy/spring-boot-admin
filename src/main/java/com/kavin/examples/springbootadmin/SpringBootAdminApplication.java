576dd8d2e5ae48586f9ad4db298f314e5a317038
Kavin <kavinduraisamy91@gmail.com>
2018-04-27 14:00:36 +0530

    first commit

---
+++ src/main/java/com/kavin/examples/springbootadmin/SpringBootAdminApplication.java
@@ -0,0 +1,19 @@
+package com.kavin.examples.springbootadmin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Configuration;
+
+import de.codecentric.boot.admin.config.EnableAdminServer;
+
+@Configuration
+@EnableAutoConfiguration
+@EnableAdminServer
+@SpringBootApplication
+public class SpringBootAdminApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootAdminApplication.class, args);
+	}
+}
